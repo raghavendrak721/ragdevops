ff677a04f5671cf80dc345816a3dd782954873ee
raghavendrak721 <74825360+raghavendrak721@users.noreply.github.com>
2023-08-01 09:51:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,25 @@
- # The configuration for the `remote` backend.
-#     terraform {
-#       backend "remote" {
-#         # The name of your Terraform Cloud organization.
-#         organization = "ust"
-#
-#         # The name of the Terraform Cloud workspace to store Terraform state files in.
-#         workspaces {
-#           name = "raghav"
-#         }
-#       }
-#     }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+  access_key = "AKIAV66CJVUUZF2CEJYE"
+  secret_key = "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"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0ded8326293d3201b"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "RaghavEc2"
+  }
+}
